bbb849ab734200b9e92f363856b19830015c62f7
Vyom Desai <hypersync112@gmail.com>
2020-10-03 13:53:49 +0530

    Connect to DataBase

--- server.js
+++ server.js
@@ -4,18 +4,32 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-// declarations
+// connections
 const app = express();
 const PORT = 4000;
+mongoose.connect(
+  "mongodb+srv://admin:mixedfruit@cluster0.e9wwo.mongodb.net/pc-spec-db?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+const connection = mongoose.connection;
 
 // middlewares
 app.use(cors());
 app.use(bodyParser.json());
+connection.once("open", () => {
+  console.log("----------------------------------");
+  console.log("Connection established with DataBase");
+  console.log("----------------------------------");
+});
 
 // routes
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World! the server is alive!!!")
-})
+  res.status(200).send("Hello World! the server is alive!!!");
+});
 
 // listener
 app.listen(PORT, () => {
