a3dea1c92793bb63a0062d73a6fbd95f1e0bf41a
Vyom Desai <hypersync112@gmail.com>
2020-10-15 01:28:16 +0530

    [1/2]Connect Frontend to Backend

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 // imports
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 
 // connections
 const app = express();
 const PORT = 4000;
-mongoose.connect(
-  "mongodb+srv://admin:mixedfruit@cluster0.e9wwo.mongodb.net/pc-spec-db?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
-const connection = mongoose.connection;
 
 // middlewares
 app.use(cors());
 app.use(bodyParser.json());
-connection.once("open", () => {
-  console.log("----------------------------------");
-  console.log("Connection established with DataBase");
-  console.log("----------------------------------");
-});
 
 // routes
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World! the server is alive!!!");
+app.post("/estimateServer", (req, res) => {
+  res.status(200).send("Data recieved");
+  const newBuild = {
+    budget: req.body.budget,
+    games: req.body.games,
+    extras: req.body.extras,
+  };
+  console.log(newBuild);
 });
 
 // listener
